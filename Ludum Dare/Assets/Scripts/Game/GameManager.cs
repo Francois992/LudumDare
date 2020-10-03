270a83b086521c8c2d7068a93fe64119d7d7427b
Clarence <diyobe2207@gmail.com>
2020-10-03 19:26:21 +0200

    Started GameManager

---
+++ Ludum Dare/Assets/Scripts/Game/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public float timer = 15f;
+    public int nbOfTimeLoops = 15;
+
+
+    
+
+    IEnumerator UpdateTimer()
+    {
+        //takingAway = true;
+
+        string minutes = ((int)timer / 60).ToString("00");
+        string seconds = (timer % 60).ToString("00");
+
+        //timerText.text = minutes + ":" + seconds;
+        yield return new WaitForSeconds(1);
+        timer -= 1;
+        //takingAwaky = false;
+    }
+}
