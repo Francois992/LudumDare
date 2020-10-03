04984bfcffd076a5b11f78efec3325f7c700bbe2
François <dessartsfran@gmail.com>
2020-10-03 13:07:25 +0200

    Button + moving Plateform + door

---
+++ Ludum Dare/Assets/Scripts/Button.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    [SerializeField] private Activatable activatable;
+
+    public Action Activate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Activate += activatable.OnActivate;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            Activate();
+        }
+    }
+}
