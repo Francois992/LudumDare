c6a6d4ed278521d40e9768da453b6029b1c75443
François <dessartsfran@gmail.com>
2020-10-03 17:56:25 +0200

    Pushable objects

--- Ludum Dare/Assets/Scripts/Character/Character.cs
+++ Ludum Dare/Assets/Scripts/Character/Character.cs
@@ -14,6 +14,9 @@ public class Character : MonoBehaviour
 
     public float jumpForce = 400f;
     public float speed = 10.0f;
+    private float normalSpeed = 10.0f;
+
+    public float speedPush = 5.0f;
     public float fallSpeed = 10.0f;
     public LayerMask layerMask;
 
@@ -24,6 +27,11 @@ public class Character : MonoBehaviour
 
     Vector3 initialGravity;
 
+    private Vector3 OrientationFacing = Vector3.right;
+
+    private bool isPushing = false;
+    private Pushable pushedObject;
+
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
@@ -31,6 +39,7 @@ public class Character : MonoBehaviour
 
     private void Start()
     {
+        normalSpeed = speed;
         initialGravity = Physics.gravity;
         Physics.gravity = new Vector3(initialGravity.x, Physics.gravity.y * 3, initialGravity.z);
     }
@@ -44,7 +53,20 @@ public class Character : MonoBehaviour
             jump = true;
         }
 
+        if (horizontalMove > 0) OrientationFacing = Vector3.right;
+        else if (horizontalMove < 0) OrientationFacing = Vector3.left;
 
+        CheckForPushable();
+
+        if (isPushing)
+        {
+            if (!Input.GetKey(KeyCode.E))
+            {
+                isPushing = false;
+                speed = normalSpeed;
+                pushedObject = null;
+            }
+        }
     }
 
     void FixedUpdate()
@@ -53,6 +75,24 @@ public class Character : MonoBehaviour
         Jump();
         jump = false;
         //UpdateGravity();
+    }
+
+    private void CheckForPushable()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, OrientationFacing, out hit, .5f))
+        {
+            if (hit.transform.CompareTag("Pushable"))
+            {
+                if (Input.GetKey(KeyCode.E))
+                {
+                    isPushing = true;
+                    pushedObject = hit.transform.GetComponent<Pushable>();
+                }
+            }
+        }
+       
     }
 
     private void Jump()
@@ -82,6 +122,12 @@ public class Character : MonoBehaviour
     void Move(float dirX)
     {
         rb.MovePosition(new Vector2(transform.position.x + (dirX * Time.fixedDeltaTime), transform.position.y));
+
+        if (isPushing)
+        {
+            speed = speedPush;
+            pushedObject.transform.position = Vector3.MoveTowards(pushedObject.transform.position, new Vector2(pushedObject.transform.position.x + horizontalMove, pushedObject.transform.position.y), speed *Time.deltaTime);
+        }
         //rb.velocity = new Vector2(rb.velocity.x, verticalSpeed);
     }
 
